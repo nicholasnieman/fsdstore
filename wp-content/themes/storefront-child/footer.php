40124158fbefb2c6385d898f5928d0db8ac8e38d
Nicholas Nieman <nnieman@fsdae.com>
2018-02-19 17:13:59 -0500

    update footer

---
+++ wp-content/themes/storefront-child/footer.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package storefront
+ */
+
+?>
+
+		</div><!-- .col-full -->
+	</div><!-- #content -->
+
+	<?php do_action( 'storefront_before_footer' ); ?>
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="col-full">
+	        <p style="text-align: center;">&copy; <?php echo DATE('Y');?> <a href="https://www.framelessshowerdoors.com" target="_blank">The Original Frameless Shower Doors</a>. All Rights Reserved. | <a href="<?php bloginfo('url'); ?>/terms-conditions/">Terms &amp; Conditions</a></p>
+
+
+		</div><!-- .col-full -->
+	</footer><!-- #colophon -->
+
+	<?php do_action( 'storefront_after_footer' ); ?>
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
