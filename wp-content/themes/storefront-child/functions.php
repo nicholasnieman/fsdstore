9321b82111df3d4a2ddd59ec7959d87a53500b61
Nicholas Nieman <nnieman@fsdae.com>
2018-02-27 17:25:00 -0500

    update menu styles

--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -1,9 +1,7 @@
 <?php
 
 function my_theme_enqueue_styles() {
-
     $parent_style = 'storefront';
-
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'storefront-child',
         get_stylesheet_directory_uri() . '/style.css',
@@ -12,15 +10,10 @@ function my_theme_enqueue_styles() {
     );
 }
 
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-
 function remove_header_content() {
 	remove_action( 'storefront_header', 'storefront_site_branding', 20 );
-
 	remove_action( 'storefront_header', 'storefront_skip_links', 0 );
 }
-
-add_action('init', 'remove_header_content');
 
 function my_remove_variation_price( $price ) {
 $price = '';
@@ -29,5 +22,13 @@ return $price;
 
 add_filter( 'woocommerce_variable_sale_price_html', 'my_remove_variation_price', 10, 2 );
 add_filter( 'woocommerce_variable_price_html', 'my_remove_variation_price', 10, 2 );
+
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+add_action('init', 'remove_header_content');
+
+remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20 );
+
+
+
  
 
