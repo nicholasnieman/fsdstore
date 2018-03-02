7f81534675979588a40daf1b041758c5783ed76d
Nicholas Nieman <nnieman@fsdae.com>
2018-03-02 11:16:22 -0500

    add image header

--- wp-content/themes/storefront-child/header.php
+++ wp-content/themes/storefront-child/header.php
@@ -31,8 +31,7 @@
 		<div class="col-full">
 		<div id="branding-container">
 			<a href="https://www.framelessshowerdoors.com" target="_blank"><img id="logo" src="<?php echo site_url(); ?>/wp-content/uploads/2018/02/FSDlogo.png" alt=""></a>
-<!-- 			<h1 id="site-heading">Online Parts Warehouse</h1>
- -->		</div>
+		</div>
 			
 			<?php
 			/**
