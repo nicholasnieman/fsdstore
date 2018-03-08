5be3ce14f9dac6afbc5fd91b1adc21add05551cc
Nicholas Nieman <nnieman@fsdae.com>
2018-03-08 14:55:45 -0500

    remove product dimensions and weight from additional information tab

--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -22,6 +22,7 @@ return $price;
 
 add_filter( 'woocommerce_variable_sale_price_html', 'my_remove_variation_price', 10, 2 );
 add_filter( 'woocommerce_variable_price_html', 'my_remove_variation_price', 10, 2 );
+add_filter( 'wc_product_enable_dimensions_display', '__return_false' );
 
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 add_action('init', 'remove_header_content');
