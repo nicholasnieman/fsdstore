da1801dec44e1adc4a41e757679447878e4f39bf
Nicholas Nieman <nnieman@fsdae.com>
2018-03-12 13:05:49 -0400

    add glass header

--- wp-content/themes/storefront-child/functions.js
+++ wp-content/themes/storefront-child/functions.js
@@ -1,1 +1,13 @@
+// function changeBackground() {
+// 	var header = document.getElementById("masthead");
+// 	var offset = window.pageYOffset;
+// 	if (offset > 235) {
+// 		header.style.position = "absolute";
+// 		header.style.top = "235px";
+// 	} else {
+// 		header.style.position = "fixed";
+// 		header.style.top = "0";
+// 	}
+// }
 
+// window.addEventListener('scroll', changeBackground);
