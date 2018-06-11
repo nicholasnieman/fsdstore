8eeee42edb7d66775943c8ef6a242833c4123d22
Nicholas Nieman <nnieman@fsdae.com>
2018-06-11 13:58:29 -0400

    update google tag manager code

--- wp-content/themes/storefront-child/header.php
+++ wp-content/themes/storefront-child/header.php
@@ -23,7 +23,7 @@
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NB5K7T');</script>
+})(window,document,'script','dataLayer','GTM-KPT2T7W');</script>
 <!-- End Google Tag Manager -->
 
 </head>
@@ -32,7 +32,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 <body <?php body_class(); ?>>
 
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NB5K7T"
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KPT2T7W"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
 
